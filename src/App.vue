e1042b0a66040c5cf1e014476122f924eacad836
Hammam GNF <hammamgonjil@gmail.com>
2026-01-22 13:55:58 +0700

    refactor: bootstrap auth state on app mount

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
+<template>
+  <router-view />
+</template>
+
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
+import { onMounted } from "vue";
+import { useAuthStore } from "./stores/auth.store";
+
+const auth = useAuthStore();
+
+onMounted(async () => {
+  if (auth.token && !auth.user) {
+    try {
+      await auth.fetchMe();
+    } catch {
+      auth.logout();
+    }
+  }
+});
 </script>
 
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
 <style scoped>
 .logo {
