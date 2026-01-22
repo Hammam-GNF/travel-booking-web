f08fefc257f1002c7b4f7b4df82d27ed7199abbc
Hammam GNF <hammamgonjil@gmail.com>
2026-01-22 14:31:50 +0700

    chore(app): initialize auth state on app startup

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/auth.store';
 
-createApp(App)
-  .use(createPinia())
-  .use(router)
-  .mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+
+const auth = useAuthStore();
+auth.initAuth();
+
+app.use(router);
+app.mount("#app");
